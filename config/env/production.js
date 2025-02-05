aa6365fe41b0a07d8960d084a382b5f942a54248
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2025-02-05 23:04:13 +0100

    Update production configuration to use environment variables for database URLs and base URL

--- config/env/production.js
+++ config/env/production.js
@@ -245,10 +245,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+     onlyAllowOrigins: [
+       'https://anagrafica.asp.robertodedomenico.it',
+     ],
 
 
     /***************************************************************************
