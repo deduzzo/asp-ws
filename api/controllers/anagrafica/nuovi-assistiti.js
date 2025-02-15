590638512cb7412ce1de83d11aa54660a782993e
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2025-02-15 16:23:04 +0100

    Update lastCheck assignment to use inputs.assistito in assistito existence check

--- api/controllers/anagrafica/nuovi-assistiti.js
+++ api/controllers/anagrafica/nuovi-assistiti.js
@@ -17,25 +17,97 @@ module.exports = {
       type: 'ref',
       description: 'Array di assistiti da inserire (omettere id, createdAt, updatedAt e md5)',
       required: true,
+      example: {
+        array: [
+          {
+            cf: 'RSSMRA80A01H501V',
+            cfNormalizzato: 'RSSMRA80A01H501V',
+            cognome: 'Rossi',
+            nome: 'Mario',
+            sesso: 'M',
+            dataNascita: 315446400,
+            comuneNascita: 'Roma',
+            codComuneNascita: 'H501',
+            codIstatComuneNascita: '058091',
+            provinciaNascita: 'RM',
+            indirizzoResidenza: 'Via Roma 1',
+            capResidenza: '00100',
+            comuneResidenza: 'Roma',
+            codComuneResidenza: 'H501',
+            codIstatComuneResidenza: '058091',
+            asp: '201',
+            ssnTipoAssistito: 'R',
+            ssnInizioAssistenza: 1577836800,
+            ssnFineAssistenza: 1735689600,
+            ssnMotivazioneFineAssistenza: 'Scadenza naturale',
+            ssnNumeroTessera: '80123456',
+            MMGUltimaOperazione: 'SCELTA',
+            MMGUltimoStato: 'ATTIVO',
+            MMGTipo: 'MMG',
+            MMGCodReg: '123456',
+            MMGNome: 'Giuseppe',
+            MMGCognome: 'Bianchi',
+            MMGCf: 'BNCGPP70B01H501X',
+            MMGDataScelta: 1577836800,
+            MMGDataRevoca: null,
+            dataDecesso: null
+          },
+          {
+            cf: 'VRDLCU85M41H501Y',
+            cfNormalizzato: 'VRDLCU85M41H501Y',
+            cognome: 'Verdi',
+            nome: 'Lucia',
+            sesso: 'F',
+            dataNascita: 493344000,
+            comuneNascita: 'Roma',
+            codComuneNascita: 'H501',
+            codIstatComuneNascita: '058091',
+            provinciaNascita: 'RM',
+            indirizzoResidenza: 'Via Napoli 42',
+            capResidenza: '00100',
+            comuneResidenza: 'Roma',
+            codComuneResidenza: 'H501',
+            codIstatComuneResidenza: '058091',
+            asp: '201',
+            ssnTipoAssistito: 'R',
+            ssnInizioAssistenza: 1609459200,
+            ssnFineAssistenza: 1767225600,
+            ssnMotivazioneFineAssistenza: 'Scadenza naturale',
+            ssnNumeroTessera: '80789012',
+            MMGUltimaOperazione: 'SCELTA',
+            MMGUltimoStato: 'ATTIVO',
+            MMGTipo: 'MMG',
+            MMGCodReg: '789012',
+            MMGNome: 'Maria',
+            MMGCognome: 'Neri',
+            MMGCf: 'NREMRA75C41H501Z',
+            MMGDataScelta: 1609459200,
+            MMGDataRevoca: null,
+            dataDecesso: null
+          }
+        ]
+      },
       custom: (value) => {
-        return Array.isArray(value) && value.length >0 && value.length <100 && value.every(async item => {
-          return await sails.helpers.isValidModel.with({
-            item: item,
-            modelName: 'anagrafica_assistiti'
+        return Array.isArray(value) &&
+          value.length > 0 &&
+          value.length < 100 &&
+          value.every(async item => {
+            return await sails.helpers.isValidModel.with({
+              item: item,
+              modelName: 'anagrafica_assistiti'
+            });
           });
-        });
       }
-    },
+    }
   },
-
   exits: {},
-
   fn: async function (inputs) {
     let assistitoCreato = null;
     const res = this.res;
     const responses = [];
     const toDelete = ['id', 'createdAt', 'updatedAt', 'md5', 'eta', 'lastCheck'];
     let someError = false;
+
     for (let assistito of inputs.assistiti) {
       if (!assistito.cf) {
         responses.push({
@@ -47,18 +119,19 @@ module.exports = {
         someError = true;
         break;
       }
+
       try {
-        // remove if exists from assistito the fields: id, createdAt, updatedAt, md5
         toDelete.forEach(field => {
           if (assistito[field]) {
             delete assistito[field];
           }
         });
         assistito['md5'] = utils.calcolaMD5daStringa(JSON.stringify(assistito));
-        // verify that exist assistito with cf and md5
+
         let assistitoEsistente = await Anagrafica_Assistiti.findOne({
           cf: assistito.cf,
         });
+
         if (assistitoEsistente) {
           assistito.lastCheck = new Date();
           if (assistitoEsistente.md5 === assistito.md5) {
@@ -73,7 +146,7 @@ module.exports = {
             });
             break;
           }
-          //update the existing assistito
+
           await Anagrafica_Assistiti.updateOne({
             id: assistitoEsistente.id,
           }).set(assistito);
@@ -85,6 +158,7 @@ module.exports = {
           });
           break;
         }
+
         assistitoCreato = await Anagrafica_Assistiti.create(assistito).fetch();
       } catch (err) {
         responses.push({
@@ -97,6 +171,7 @@ module.exports = {
         someError = true;
         break;
       }
+
       responses.push({
         assistito: assistito.cf,
         statusCode: 200,
@@ -104,6 +179,7 @@ module.exports = {
         msg: 'Assistito ' + assistitoCreato.cf + ' creato con successo con md5: ' + assistitoCreato.md5,
       });
     }
+
     if (someError) {
       return res.ApiResponse({
         errType: ERROR_TYPES.MULTIPLE_ERRORS,
