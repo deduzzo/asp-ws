e111efb6cc39d639dd3de1e1780ac24c744b8535
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2025-03-13 08:30:12 +0100

    Enhance geolocation data retrieval by adding criteria for filtered results and improving response handling

--- api/controllers/anagrafica/get-geo-data.js
+++ api/controllers/anagrafica/get-geo-data.js
@@ -6,15 +6,10 @@
  *     - Anagrafica
  */
 
+const {ERROR_TYPES} = require("../../responses/ApiResponse");
 module.exports = {
-
-
-  friendlyName: 'Get geo data',
-
-
-  description: '',
-
-
+  friendlyName: 'Ottieni i dati geografici degli assistiti per comune di residenza',
+  description: 'Ottieni i dati geografici degli assistiti per comune di residenza',
   inputs: {
     // codComuneResidenza
     codComuneResidenza: {
@@ -26,23 +21,44 @@ module.exports = {
       type: 'boolean',
       description: 'Se true, ritorna solo i record con geolocazione precisa',
       defaultsTo: false
+    },
+    onlyGeoloc: {
+      type: 'boolean',
+      description: 'Se true, ritorna solo i record geolocalizzati',
+      defaultsTo: false
     }
   },
-
-
   exits: {},
 
   fn: async function (inputs) {
     const res = this.res;
+    let criteria = {};
+    let campi = ['cf', 'capResidenza', 'indirizzoResidenza', 'lat', 'long', 'geolocPrecise'];
+    if (inputs.onlyGeoloc === true)
+      criteria.lat = {'!=': null};
+    if (inputs.onlyGeolocationPrecise === true)
+      criteria = {...criteria, onlyGeolocationPrecise: true};
+    if (Object.keys(criteria).length === 0) { // indirizzoResidenza not null OR cap not null
+      criteria = {
+        or: [
+          {indirizzoResidenza: {'!=': null}},
+          {capResidenza: {'!=': null}}
+        ]
+      };
+    }
     let data = await Anagrafica_Assistiti.find({
       codComuneResidenza: inputs.codComuneResidenza,
-      lat: {'!=': null},
-    }).select(['cf', 'lat', 'long', 'geolocPrecise']);
+      ...criteria
+    }).select(campi);
     const cleanedData = data.map(item => _.omit(item, ['createdAt', 'updatedAt']));
-    return res.ApiResponse({
-      data: cleanedData
-    });
+    if (data.length === 0)
+      return res.ApiResponse({
+        errType: ERROR_TYPES.NOT_FOUND,
+        errMsg: 'Nessun assistito trovato'
+      });
+    else
+      return res.ApiResponse({
+        data: cleanedData
+      });
   }
-
-
 };
